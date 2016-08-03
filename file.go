01b529bb3da555e89a7478e84a78a63c9e6014b6
crasm <crasm@vczf.io>
2016-08-02 23:10:48 -0400

    Fixed (and made consistent) copyright notice

--- file.go
+++ file.go
@@ -1,3 +1,6 @@
+// Copyright (c) 2016, crasm <crasm@vczf.io>
+// This code is open source under the ISC license. See LICENSE for details.
+
 package main
 
 import "os"
--- util.go
+++ util.go
@@ -1,7 +1,7 @@
-package main
-
 // Copyright (c) 2016, crasm <crasm@vczf.io>
 // This code is open source under the ISC license. See LICENSE for details.
+
+package main
 
 import "fmt"
 
